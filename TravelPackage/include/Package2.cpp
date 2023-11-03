369b1cc9fe018dcbd31a712dc466aaa074ec14d2
Minhyuk Hong <108979014+MinhyukHong@users.noreply.github.com>
2023-11-03 00:48:04 -0500

    Add files via upload

---
+++ TravelPackage/include/Package2.cpp
@@ -0,0 +1,16 @@
+#include "Package2.h"
+#include <iostream>
+
+using namespace std;
+
+void Package2::bookHotels() {
+    cout << "Hotels are booked." << endl;
+}
+
+void Package2::bookFlights() {
+    cout << "Flights are booked." << endl;
+}
+
+void Package2::bookTours() {
+    cout << "Tours are booked." << endl;
+}
